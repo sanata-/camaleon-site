13fbdeeb17ad05d2a9d94028d969e01fe366857b
sanata- <x66w@yandex.ru>
2016-02-22 18:14:38 +0300

    Add sections

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,14 @@
+    //= require modernizr
     //= require jquery
     //= require materialize-sprockets
     //= require jquery_ujs
     //= require turbolinks
     //= require_tree .
-    $(document).ready(function () {
-      $(".dropdown-button").dropdown();
+$( document ).ready(function(){
       $(".button-collapse").sideNav();
-     });
+      $('ul.tabs').tabs('select_tab', 'tab_id');
+       $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+
+});
